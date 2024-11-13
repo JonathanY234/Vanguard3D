11ce675e760e0b0b08a0b401523d8a91e784830f
Jonathan <j.e.young@talktalk.net>
2024-11-13 23:17:34 +0000

    fixed too tall walls, still needs work though

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -9,8 +9,8 @@
 #include "load_textures.h"
 
 static Uint32 floorColour = 0xFF606060;
-static Uint32 blue = 0xFF6464C8;
-static Uint32 offBlue = 0xFF0000FF;
+//static Uint32 blue = 0xFF6464C8;
+//static Uint32 offBlue = 0xFF0000FF;
 static Uint32 ceilingColour = 0xFF404040;
 
 
@@ -18,39 +18,18 @@ static Uint32 ceilingColour = 0xFF404040;
 // frequently used so needs to be performant
 static void setPixel(int x, int y, Uint32 colour) {
     Uint32* pixels = (Uint32*)backBuffer->pixels;
+    if (y < 0 || y >= backBuffer->h) {
+    // Skip setting pixel if out of bounds
+        return;
+    }
     pixels[(y * backBuffer->w) + x] = colour;
 }
 
-//static void drawColumn(int x, int wallHeight, int wallType) {
-//    if (wallHeight > Settings::getScreenHeight()) {
-//        wallHeight = Settings::getScreenHeight();
-//    }
-//    if (wallHeight < 0) wallHeight = 0;
-//
-//    Uint32 wallColour;
-//    if (wallType == 0) {
-//        wallColour = blue;
-//    } else { //if (wallType == 1) {
-//        wallColour = offBlue;
-//    }
-//
-//    int screenHeight = Settings::getScreenHeight();
-//    int floorTop = (screenHeight-wallHeight) /2;
-//    int wallTop = floorTop + wallHeight;
-//
-//    for (int i=0; i < floorTop; i++) {
-//        setPixel(x, i, floorColour);
-//    }
-//    for (int i=floorTop; i < wallTop; i++) {
-//        setPixel(x, i, wallColour);
-//    }
-//    for (int i=wallTop; i < screenHeight; i++) {
-//        setPixel(x, i, ceilingColour);
-//    }
-//}
 static void drawColumn(int x, int wallHeight, int wallType, double xPosWithinTexture, int wallnum) { // implement draw textured column
+    int overheight = 0;
     if (wallHeight > Settings::getScreenHeight()) { //stops crashing but need proper solution
-        wallHeight = Settings::getScreenHeight();
+        overheight = (wallHeight - Settings::getScreenHeight() / 2);
+        //wallHeight = Settings::getScreenHeight();
     }
     if (wallHeight < 0) wallHeight = 0;
 
@@ -61,18 +40,28 @@ static void drawColumn(int x, int wallHeight, int wallType, double xPosWithinTex
     for (int i=0; i < floorTop; i++) {
         setPixel(x, i, floorColour);
     }
+    // draw
     // copy or refrence/pointer????
-    std::vector<Uint32> column = textures[wallnum]->getColumn(xPosWithinTexture);
+    const std::vector<Uint32>& column = textures[wallnum]->getColumn(xPosWithinTexture);
     double pixel_gap = 100.0 / wallHeight;
 
-    for (int i=floorTop; i < wallTop; i++) { //this can be optimised
-        int temp = std::round((i-floorTop)*pixel_gap);
-        //setPixel(x, i, column[]);
-        if (temp >= 0 && temp < wallHeight) {
-            setPixel(x, i, column[temp]);
-        }
+    int max_temp = 0;
+    if (overheight > 1) {
+        std::cout << "overheight" << std::endl;
     }
-    setPixel(x, xPosWithinTexture*100, 0xFFFF0000);
+
+    for (int i=floorTop; i < wallTop; i++) { //this should be optimised, this loop is looping too wide a range
+        int temp = std::round((i-floorTop)*pixel_gap);//    also experiment with more "up" taller walls
+        //setPixel(x, i, column[]);
+        //if (temp >= 0 && temp < wallHeight) {
+
+        //std::cout << "1" << std::endl;
+        setPixel(x, i, column[temp]);
+        //std::cout << "2" << std::endl;
+        //}
+    }
+
+    //setPixel(x, xPosWithinTexture*100, 0xFFFF0000);
     //
     //        for (int x=0;x<100;x++) {
     //        std::vector<Uint32> vect = text1->getColumn(static_cast<double>(x) / 100);
